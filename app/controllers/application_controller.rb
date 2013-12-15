23dc979c468e3476d28268db021bf9ce431626c6
D瓜哥 <leejun119@gmail.com>
2013-12-15 14:43:14 +0800

    完成权限配置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # 为首个用户赋予admin角色
+  def  after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      if User.count == 1
+        resource.add_role 'admin'
+      end
+      resource
+    end
+    root_path
+  end
 end

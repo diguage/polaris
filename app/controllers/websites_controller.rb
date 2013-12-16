98b80d48b1d0f9ef75b629183b92a39dc1b54219
D瓜哥 <leejun119@gmail.com>
2013-12-16 23:55:31 +0800

    增加权限控制：用户必须登录才能操作

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,4 +1,5 @@
 class WebsitesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_website, only: [:show, :edit, :update, :destroy]
 
   # GET /websites
